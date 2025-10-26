66e1f7d161af3409ebf16fa87321047fba49a600
OkadaMiki <okadamiki17@gmail.com>
2025-10-26 23:21:37 +0900

    お勉強

--- src/app/develop/page.tsx
+++ src/app/develop/page.tsx
@@ -1,36 +1,56 @@
 'use client'
 
-export default function Test() {
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Page() {
+    const [inputValue, setInputValue] = useState("")
+    // set〇〇は箱にものを入れるやつ、useStateは更新したら画面を書き換えてくれるやつ
+    const [todoList, setTodoList] = useState<string[]>([])
+    // 配列が空で、どんな物が入るかわからないとエラーが出る = 型安全
+    // <string[]> 　= 型定義
+
+
+
     return (
         <>
             <div>
                 <input
                     type="text"
-                    onBlur={(e) => {
-                        console.log(e.target.value);
+                    onChange={(e) => {
+                        // onChangeは代わった瞬間に動かすやつ、onBlurはフォーカスが外れたときに動くやつ
+                        setInputValue(e.target.value)
                     }}
-
+                    value={inputValue}
+                // 送ったら空にしたい、onBlurからonChangeに変更
                 />
                 <button
                     onClick={(e) => {
-                        console.log(e);
+                        setTodoList([...todoList, inputValue])
+                        setInputValue("")
+
                     }}
                 >おくる</button>
             </div>
+            <Link href={"/develop/0001"}>
+                つぎ
+            </Link>
             <div>
                 <ul>
-                    <li>
-                        なんかでる
-                        <button>さくじょ</button>
-                    </li>
-                    <li>
-                        なんかでる
-                        <button>さくじょ</button>
-                    </li>
-                    <li>
-                        なんかでる
-                        <button>さくじょ</button>
-                    </li>
+                    {/* mapかく */}
+                    {todoList.map((todo, index) => (
+                        <li key={index}>
+                            {todo}
+                            <button
+                                onClick={() => {
+                                    setTodoList(todoList.filter((_, indexForDel) => (
+                                        // 条件のやつ
+                                        indexForDel !== index
+                                    )))
+                                }}
+                            >さくじょ</button>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </>
