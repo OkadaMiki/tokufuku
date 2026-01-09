e3b97a5c2a4e36aef448a9565a13ddc34e75b76b
Kawahara-tk <k.takumi11q2@gmail.com>
2026-01-09 09:56:35 +0900

    追加：フッタースタイル
    フッターのスタイル追加

--- src/components/layout/FooterNav.tsx
+++ src/components/layout/FooterNav.tsx
@@ -5,11 +5,26 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Link href="/list" className={styles.box}></Link>
-      <Link href="/record" className={styles.box}></Link>
-      <Link href="/home" className={styles.box}></Link>
-      <Link href="/care" className={styles.box}></Link>
-      <Link href="/user" className={styles.box}></Link>
+      <Link href="/list" className={styles.box}>
+        <img src="/assets/footer/log.svg" alt="" />
+        <span>ログ</span>
+      </Link>
+      <Link href="/record" className={styles.box}>
+        <img src="/assets/footer/record.svg" alt="" />
+        <span>記録</span>
+      </Link>
+      <Link href="/home" className={styles.box}>
+        <img src="/assets/footer/home.svg" alt="" />
+        <span>ホーム</span>
+      </Link>
+      <Link href="/care" className={styles.box}>
+        <img src="/assets/footer/dressup.svg" alt="" />
+        <span>着せ替え</span>
+      </Link>
+      <Link href="/user" className={styles.box}>
+        <img src="/assets/footer/menu.svg" alt="" />
+        <span>メニュー</span>
+      </Link>
     </footer>
   );
 }
